0c4a9ba3518fe47f2c93c84c5d4ad2a1d11c4b4c
Stanislav Liberman <lirm604@gmail.com>
2016-07-22 16:19:18 -0500

    Refactored tests

--- aeron/aeron_test.go
+++ aeron/aeron_test.go
@@ -24,41 +24,37 @@ import (
 	"time"
 )
 
-func TestAeronBasics(t *testing.T) {
+const (
+	TEST_CHANNEL = "aeron:udp?endpoint=localhost:40123"
+	TEST_STREAMID = 10
+)
 
-	logging.SetLevel(logging.INFO, "aeron")
-	logging.SetLevel(logging.INFO, "memmap")
-	logging.SetLevel(logging.INFO, "driver")
-	logging.SetLevel(logging.INFO, "counters")
-	logging.SetLevel(logging.INFO, "logbuffers")
-
-	ctx := NewContext().AeronDir("/tmp").MediaDriverTimeout(time.Second * 10)
-	a := Connect(ctx)
-	defer a.Close()
-
-	subscription := <-a.AddSubscription("aeron:udp?endpoint=localhost:40123", 10)
-	publication := <-a.AddPublication("aeron:udp?endpoint=localhost:40123", 10)
-
-	counter := 0
-	handler := func(buffer *buffers.Atomic, offset int32, length int32, header *logbuffer.Header) {
-		t.Logf("%8.d: Gots me a fragment offset:%d length: %d\n", counter, offset, length)
-		counter++
-	}
-
+func send1(pub *Publication, t *testing.T) bool {
 	message := "this is a message"
 	srcBuffer := buffers.MakeAtomic(([]byte)(message))
 	timeoutAt := time.Now().Add(time.Second * 10)
-	for publication.Offer(srcBuffer, 0, int32(len(message)), nil) <= 0 {
+	res := true
+	for pub.Offer(srcBuffer, 0, int32(len(message)), nil) <= 0 {
 		if time.Now().After(timeoutAt) {
 			t.Logf("Timed out at %v", time.Now())
+			res = false
 			break
 		}
+		time.Sleep(time.Millisecond * 100)
 	}
+	return res
+}
 
+func receive1(sub *Subscription, t *testing.T) bool {
+	counter := 0
+	handler := func(buffer *buffers.Atomic, offset int32, length int32, header *logbuffer.Header) {
+		t.Logf("%8.d: Recvd fragment: offset:%d length: %d\n", counter, offset, length)
+		counter++
+	}
 	fragmentsRead := 0
-	timeoutAt = time.Now().Add(time.Second * 10)
+	timeoutAt := time.Now().Add(time.Second * 10)
 	for {
-		fragmentsRead += subscription.Poll(handler, 10)
+		fragmentsRead += sub.Poll(handler, 10)
 		if fragmentsRead == 1 {
 			break
 		}
@@ -66,6 +62,7 @@ func TestAeronBasics(t *testing.T) {
 			t.Error("timed out waiting for message")
 			break
 		}
+		time.Sleep(time.Millisecond * 1000)
 	}
 	if fragmentsRead != 1 {
 		t.Error("Expected 1 fragment. Got", fragmentsRead)
@@ -74,14 +71,59 @@ func TestAeronBasics(t *testing.T) {
 		t.Error("Expected 1 message. Got", counter)
 	}
 
+	return counter == 1
+}
+
+func logtest(flag bool) {
+	if !flag {
+		logging.SetLevel(logging.INFO, "aeron")
+		logging.SetLevel(logging.INFO, "memmap")
+		logging.SetLevel(logging.INFO, "driver")
+		logging.SetLevel(logging.INFO, "counters")
+		logging.SetLevel(logging.INFO, "logbuffers")
+	}
+}
+
+func TestAeronBasics(t *testing.T) {
+
+	logtest(false)
+
+	ctx := NewContext().AeronDir("/tmp").MediaDriverTimeout(time.Second * 10)
+	a := Connect(ctx)
+	defer a.Close()
+
+	subscription := <-a.AddSubscription(TEST_CHANNEL, TEST_STREAMID)
+	publication := <-a.AddPublication(TEST_CHANNEL, TEST_STREAMID)
+
+	send1(publication, t)
+
+	receive1(subscription, t)
+}
+
+func TstAeronResubscribe(t *testing.T) {
+
+	logtest(false)
+
+	ctx := NewContext().AeronDir("/tmp").MediaDriverTimeout(time.Second * 10)
+	a := Connect(ctx)
+	defer a.Close()
+
+	publication := <-a.AddPublication(TEST_CHANNEL, TEST_STREAMID)
+
+	subscription := <-a.AddSubscription(TEST_CHANNEL, TEST_STREAMID)
+	send1(publication, t)
+	receive1(subscription, t)
+
+	subscription.Close()
+	subscription = nil
+
+	subscription = <-a.AddSubscription(TEST_CHANNEL, TEST_STREAMID)
+	send1(publication, t)
+	receive1(subscription, t)
 }
 
 func TestAeronClose(t *testing.T) {
-	logging.SetLevel(logging.INFO, "aeron")
-	logging.SetLevel(logging.INFO, "memmap")
-	logging.SetLevel(logging.INFO, "driver")
-	logging.SetLevel(logging.INFO, "counters")
-	logging.SetLevel(logging.INFO, "logbuffers")
+	logtest(false)
 
 	ctx := NewContext().AeronDir("/tmp").MediaDriverTimeout(time.Second * 10)
 	ctx.unavailableImageHandler = func(img *Image) {
