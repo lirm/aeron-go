d776633cb2322cfc70e0970626cead55c1a4777e
billsegall <bill@segall.net>
2021-01-25 16:59:49 +1000

    Setup archive testing
    This requires a running archive-media-driver but will not fail if one is not running.

---
+++ archive/archive_test.go
@@ -0,0 +1,41 @@
+// Copyright (C) 2021 Talos, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package archive
+
+import (
+	"testing"
+)
+
+// Rather than mock or spawn an archive-media-driver we're just seeing
+// if we can connect to one and if we can we'll run some tests. If the
+// init fails to connect then we'll skip the tests
+var context *ArchiveContext
+var archive *Archive
+
+func init() {
+	context = NewArchiveContext()
+	context.RequestChannel = *TestConfig.RequestChannel
+	context.RequestStream = int32(*TestConfig.RequestStream)
+	context.AeronDir(*TestConfig.AeronPrefix)
+	archive, _ = ArchiveConnect(context)
+}
+
+// An archive version of basic_publisher in test format
+// FIXME: requires a subscriber
+func TestConnection(t *testing.T) {
+	if archive == nil {
+		t.Log("Skipping test connection")
+	}
+}
