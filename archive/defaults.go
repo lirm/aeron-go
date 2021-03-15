0e2990d0dc588e7f820b0ab5840fc578b2a5412a
billsegall <bill@segall.net>
2021-03-15 12:54:50 +1000

    Add ReceordingEvents stream/channel, and re-order

--- archive/defaults.go
+++ archive/defaults.go
@@ -17,36 +17,39 @@ package archive
 import (
 	"github.com/lirm/aeron-go/aeron/idlestrategy"
 	logging "github.com/op/go-logging"
-
 	"time"
 )
 
 // FIXME: Provide a method to return the defaults and initialize with parameterised version
 
 type Defaults struct {
-	ArchiveLoglevel       logging.Level
-	AeronLoglevel         logging.Level
-	ControlTimeout        time.Duration      // How long to try sending control messages [see Proxy.Timeout]
-	ControlIdleStrategy   idlestrategy.Idler // Idlestrategy as for aeron itself
-	RecordingIdleStrategy idlestrategy.Idler // Idlestrategy as for aeron itself
-	ControlRetries        int                // How many retries for control messages [see Proxy.Retries] // FIXME: use
-	RangeChecking         bool               // Turn on range checking for control protocol marshalling
-	ResponseChannel       string             // control response subscription channel
-	ResponseStream        int32              // and stream
-	RequestChannel        string             // control request publication channel
-	RequestStream         int32              // and stream
+	ArchiveLoglevel        logging.Level
+	AeronLoglevel          logging.Level
+	ControlTimeout         time.Duration      // How long to try sending/receiving control messages [see Proxy.Timeout]
+	ControlIdleStrategy    idlestrategy.Idler // Idlestrategy as for aeron itself
+	ControlRetries         int                // How many retries for control messages [see Proxy.Retries] // FIXME: use
+	RangeChecking          bool               // Turn on range checking for control protocol marshalling
+	RequestChannel         string             // Control request publication channel
+	RequestStream          int32              // and stream
+	ResponseChannel        string             // Control response subscription channel
+	ResponseStream         int32              // and stream
+	RecordingEventsChannel string             // Recording progress events
+	RecordingEventsStream  int32              // and stream
+	RecordingIdleStrategy  idlestrategy.Idler // Idlestrategy for the recording
 }
 
 var ArchiveDefaults Defaults = Defaults{
-	ArchiveLoglevel:       logging.INFO,
-	AeronLoglevel:         logging.NOTICE,
-	ControlTimeout:        time.Second * 5,
-	ControlIdleStrategy:   idlestrategy.Sleeping{SleepFor: time.Millisecond * 50}, // FIXME: tune
-	RecordingIdleStrategy: idlestrategy.Sleeping{SleepFor: time.Millisecond * 50}, // FIXME: tune
-	ControlRetries:        4,
-	RangeChecking:         true, // FIXME: turn off
-	ResponseChannel:       "aeron:udp?endpoint=localhost:8020",
-	ResponseStream:        20,
-	RequestChannel:        "aeron:udp?endpoint=localhost:8010",
-	RequestStream:         10,
+	ArchiveLoglevel:        logging.INFO,
+	AeronLoglevel:          logging.NOTICE,
+	ControlTimeout:         time.Second * 5,
+	ControlIdleStrategy:    idlestrategy.Sleeping{SleepFor: time.Millisecond * 50}, // FIXME: tune
+	ControlRetries:         4,
+	RangeChecking:          true, // FIXME: turn off
+	RequestChannel:         "aeron:udp?endpoint=localhost:8010",
+	RequestStream:          10,
+	ResponseChannel:        "aeron:udp?endpoint=localhost:8020",
+	ResponseStream:         20,
+	RecordingEventsChannel: "aeron:udp?control-mode=dynamic|control=localhost:8030",
+	RecordingEventsStream:  30,
+	RecordingIdleStrategy:  idlestrategy.Sleeping{SleepFor: time.Millisecond * 50}, // FIXME: tune
 }
